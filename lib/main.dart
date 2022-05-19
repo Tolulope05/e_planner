39cf43501678d2bc6ac72082e4300d8b6d69dad0
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-19 18:47:35 +0100

    successfully created widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,23 @@ class MyhomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(
+                          tx.date.toString(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           ),
